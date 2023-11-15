0dadb969b59e9516e00ce33d855a0891f668e98a
pripriya09 <pripriya0904@gmail.com>
2023-11-14 19:39:30 -0500

    nevbar

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return(
+    <>
+
+    
+ <nev>
+    <div className="logo">
+   <img src="	https://cdn1.byjus.com/byjusweb/img/home/svg/byjus_logo.svg" alt=""></img>
+   </div>
+
+
+  <div>
+
+    <ul id="nevbar">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="index.html">About</a></li>
+        <li><a href="index.html">Info</a></li>
+        <li><a href="index.html">Cont</a></li>
+        <li><a href="index.html">Help</a></li>
+    </ul>
+  </div>
+
+   </nev> 
+
+    </>
+)
+
+
+
+
+}
+export default App
